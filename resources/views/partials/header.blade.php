f4b206a8210b21c60bbbd69c15cf9fe841081393
Mirko <mirkocaccavone03@gmail.com>
2025-05-13 12:47:19 +0200

    [feat] restructure header with navigation links and logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,40 @@
 <header>
-    <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="">
-    <nav>
-        questi sono i link
-    </nav>
+    <div class="header-container center-container">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo" class="img-fluid" style="max-height: 50px;">
+
+        <nav>
+            <ul class="nav-container">
+                <li class="nav-item">
+                    <a href="#" class="nav-link custom-link px-3">CHARACTERS</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link custom-link px-3">COMICS</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link custom-link px-3">MOVIES</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link custom-link px-3">TV</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link custom-link px-3">GAMES</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link custom-link px-3">COLLECTIBLES</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link custom-link px-3">VIDEOS</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link custom-link px-3">FANS</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link custom-link px-3">NEWS</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link custom-link px-3">SHOP</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
