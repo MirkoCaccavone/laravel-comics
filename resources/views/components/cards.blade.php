10b004ebb237914011428a73e706bab7a7c5158f
Mirko <mirkocaccavone03@gmail.com>
2025-05-13 12:46:42 +0200

    [feat] add card and jumbotron components

---
+++ resources/views/components/cards.blade.php
@@ -0,0 +1,4 @@
+<div class = "container-card">
+        <img src="{{$immagine}}" alt="">
+         <h4>{{$titolo}}</h4>
+</div>
---
+++ resources/views/components/jumbo.blade.php
@@ -0,0 +1,3 @@
+<div class="jumbotron">
+    <img src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="DC Comics Jumbotron" class="w-100">
+</div>
